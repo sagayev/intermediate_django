3235c464b3c3068fe26eb965bbf95eafc8eca194
Samir Ağayev <Samir_Agayev@cbar.az>
2019-11-02 13:33:47 +0400

    Created view and forms

--- notepad/views.py
+++ notepad/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NoteModelForm
 
 # Create your views here.
+from .models import Note
+
+def create_view(request):
+    form = NoteModelForm(request.POST or None, request.FILES or None)
+    if form.is_valid:
+        form.instance.user = request.user
+        form.save()
+        return redirect('/')
+
+    context = {
+        'form' = form
+    }
+    return(request, "notepad/create.html", context)
