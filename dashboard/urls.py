af661de8859f741f1468afa3913488ad7b46544b
Samir Ağayev <Samir_Agayev@cbar.az>
2019-11-06 16:06:02 +0400

    Completed CRUD cycle for Notepad app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('notes/', include('notepad.urls'))
+    path('notes/', include('notepad.urls')),
+    path('glossary/', include('django_glossary.urls')),
 ]
